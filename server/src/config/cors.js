08d6764061b0e51d73fe66aff61e2fed296dfa66
Vinay Sagar <vinaysagar4445@gmail.com>
2025-03-18 12:34:54 +0530

    add the aws frontend endpoint

--- server/src/config/cors.js
+++ server/src/config/cors.js
@@ -2,7 +2,8 @@ import cors from 'cors';
 
 const allowedOrigins = [
   'http://localhost:3000', // Local development
-  'https://interview-nu-weld.vercel.app' // Replace with your actual Vercel domain
+  'https://interview-nu-weld.vercel.app', // Replace with your actual Vercel domain
+  "https://master.d2uy7h19d22ty2.amplifyapp.com"
 ];
 
 const corsOptions = {
