f1737c89f19ab9fe69d49b5b300f1b5abd227f3d
Vinay Sagar <vinaysagar4445@gmail.com>
2025-06-29 01:57:42 +0530

    remove the server files.

--- server/src/routes/chatRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import auth from "../middleware/firebase.js";
-import chatController from "../controllers/chatController.js";
-const chatRoutes = Router();
-
-chatRoutes.post('/', auth, chatController.chat_post)
-
-export default chatRoutes;
